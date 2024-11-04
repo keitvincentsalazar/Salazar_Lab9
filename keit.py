6b88f22f507a1774d78ee1cd654b60a59c93b8fb
keitvincentsalazar <easgodsalazar@gmail.com>
2024-11-04 20:38:59 +0800

    Update keit.py

--- keit.py
+++ keit.py
@@ -1,5 +1,13 @@
-slices = 6
-while slices > 0:
-    print(f"There are {slices} slices of pizza left.")
-    slices -= 1
-print("All slices are gone! Good stuffs!")
+rows = int(input("Enter the number of rows: "))  # Prompt user for the number of rows
+
+# Loop in each row
+for i in range(1, rows + 1):
+    # Print spaces to center the pyramid
+    for space in range(rows - i):
+        print(" ", end="")  # Print spaces for alignment
+    
+    # Print asterisks for the current row
+    for star in range(2 * i - 1):
+        print("*", end="")  # Print asterisks
+    # Move to the next line after print a row
+    print()
