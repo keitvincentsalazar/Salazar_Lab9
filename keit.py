83faa564066846cb71b28c286774dbcacfc7defb
Keit Vincent E. Salazar <easgodsalazar@gmail.com>
2024-11-04 20:33:48 +0800

    first commit

---
+++ keit.py
@@ -0,0 +1,5 @@
+slices = 6
+while slices > 0:
+    print(f"There are {slices} slices of pizza left.")
+    slices -= 1
+print("All slices are gone! Good stuffs!")
---
+++ keitttt.py
@@ -0,0 +1,3 @@
+fruits = ["apple", "banana", "cherry", "date"]
+for fruit in fruits:
+    print(f"I love eating {fruit}!")
